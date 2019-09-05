25ee14ed38a615bb65487176e939dcf05e608686
hukai <huzhengen@163.com>
2019-09-05 15:38:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(session({ secret: 'hello world', cookie: { maxAge: 6000000 } }));
 
 let allowCrossDomain = function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', req.headers.origin)
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH')
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
 	res.header('Access-Control-Allow-Headers', 'Content-Type')
 	res.header("Access-Control-Allow-Credentials", true)
 	if (req.method === "OPTIONS") {
